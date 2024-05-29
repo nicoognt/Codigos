01f3b09aa0a571be1fa57cbd7166388a9e467840
nicoognt <gnico9575@gmail.com>
2024-05-29 20:14:01 -0300

    Update 9_11.cpp

--- Struct/9_11.cpp
+++ Struct/9_11.cpp
@@ -7,24 +7,33 @@ struct ficha_alumno {
 	int dni;
 	vector<int> calificaciones;
 };
- void leer_datos(ficha_alumno){
+
+ficha_alumno leer_datos(){
 	ficha_alumno x;
 	cout<<"nombre: ";
-	cin>>nombre_alumno;
+	cin>>x.nombre_alumno;
 	cout<<"dni: ";
-	cin>>dni;
-	for(size_t i=0;i<cal.size();i++) { 
-		cin>>cal[i];
+	cin>>x.dni;
+	x.calificaciones.resize(32);
+	cout<<"notas: "<<endl;
+	for(size_t i=0;i<x.calificaciones.size();i++) { 
+		cin>>x.calificaciones[i];
 	}
- }
+	return x;
+}
 
+void meter_calif(ficha_alumno &x) {
+	x.calificaciones[6] = 8;
+	x.calificaciones[11] = 7;
+	x.calificaciones[12] = 10;
+
+}
 int main() {
- ficha_alumno n1;	
- ficha_alumno n2;	
- ficha_alumno n3;
- 
- leer_datos(ficha_alumno);
-	
+ ficha_alumno n1 = leer_datos();	
+ ficha_alumno n2 = leer_datos();	
+ ficha_alumno n3 = leer_datos();
+
+ meter_calif(n1);
 	return 0;
 }
 
