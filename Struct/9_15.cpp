66834c11b91837545e447b69e167781c9d338b9a
nicoognt <gnico9575@gmail.com>
2024-06-01 18:59:43 -0300

    Update 9_15.cpp

--- Struct/9_15.cpp
+++ Struct/9_15.cpp
@@ -10,36 +10,55 @@ using namespace std;
 struct paciente {
 	string nombre;
 	int dni;
-	string diagnos;
+	long diagnos;
 	int habitacion;
 };
 
-void completar(vector<paciente> &pacientes,int N);
-paciente x;
-int aux = 1;
-for (size_t i = 0;i<pacientes.size();i++) {
-	cout<<"Nombre: ";
-	cin>>x.nombre;
-	pacientes[i].nombre = x.nombre;
-	cout<<"DNI: ";
-	cin>>x.dni;
-	pacientes[i].dni = x.dni;
-	cout<<"Diagnóstico: ";
-	cin>>x.diagnos;
-	pacientes[i].diagnos = x.diagnos;
-	cout<<"Habitación: ";
-	cin>>x.habitacion;
-	pacientes[i].habitacion = x.habitacion;
-	aux++;
+void completar(vector<paciente> &pac) {
+	for(size_t i=0;i<pac.size();i++) { 
+		cin>>pac[i].nombre>>pac[i].dni>>pac[i].diagnos>>pac[i].habitacion;
+	}
 }
 
+int hallar_habitacion(const vector<paciente> a,string nom){
+	int auxiliar;
+	for (size_t i = 0;i < a.size();i++) {
+		if (a[i].nombre == nom) {
+			auxiliar = a[i].habitacion;
+		}
+	}
+	return auxiliar;
+}
+	
+int apendicitis(const vector<paciente> b) {
+	int cont = 0;
+	for(size_t i=0;i<b.size().size();i++) { 
+		if (b[i].diagnos == "Apendicitis") {
+			cont++;
+		}
+	}
+	return cont;
+}
+void hab_101(const vector<paciente> c) {
+	cout<<c[101].nombre<<endl;
+	cout<<c[101].dni<<endl;
+	cout<<c[101].diagnos<<endl;
+	cout<<c[101].habitacion<<endl;
+}
 int main() {
 	int N;
 	cout<<"¿Cuántos pacientes serán ingresados?: ";
 	cin>>N;
 	vector<paciente> lista(N);
-	completar(lista,N);
-	
+	completar(lista);
+	string aux;
+	cout<<"¿A qué paciente quiere encontrar?: ";
+	cin>>aux;
+	int habi = hallar_habitacion(lista,aux);
+	cout<<"La habitación de Cosme Fulanito es la "<<habi;
+	int apen = apendicitis(lista);
+	cout<<"Hay "<<apen<<" pacientes internados con Apendicitis."<<endl;
+	hab_101(lista);
 	return 0;
 }
 
